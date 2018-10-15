61500bd887edbe810a2ed7c0be02d846cbd47ea8
bsasaki <43330093+bsasaki@users.noreply.github.com>
2018-10-14 18:43:22 -0700

    Add files via upload
    up to 4.1.1

--- Header.h
+++ Header.h
@@ -4,5 +4,9 @@
 #include <fstream>
 #include <istream>
 #include <map>
+#include <vector>
+#include <string>
 
 std::map<std::string, int> first_word_counter(std::ifstream& in_file);
+std::vector<std::string> input_read(std::ifstream& in_file);
+std::map<std::string, int> map_func(std::vector<std::string>);
--- main.cpp
+++ main.cpp
@@ -6,25 +6,28 @@ Authors: Bennett Sasaki & Jordan Kam
 
 using namespace std;
 
-const int MAX_WORD_COUNT = 10000;
-
 int main() {
 	ifstream in_file;
 	std::map<std::string, int> wordCount;
 	in_file.open("L3In.txt", std::ifstream::in);
-	wordCount = first_word_counter(in_file);
+	//wordCount = first_word_counter(in_file);
+
+	std::vector<std::string> words; //Vector to hold unsorted words
+	words = input_read(in_file);
+
+	cin.get();
 
 	in_file.close();
 	return 0;
 }
 
 std::map<std::string, int> first_word_counter(ifstream& in_file) {
-/* word_counter
+/* word_counter- Section 3
    Purpose: return word count
    Input: text file to perform word count on
    Output: total word count
    Note: does not check for delimeters, only spaces.
-	   We can fix that later if we need to
+	     We can fix that later if we need to
 */
 	std::map<std::string, int> wordMap;
 	char* word = new char;
@@ -34,4 +37,27 @@ std::map<std::string, int> first_word_counter(ifstream& in_file) {
 	}
 
 	return wordMap;
+}
+
+std::vector<std::string> input_read(std::ifstream& in_file) {
+/* input_read- Section 4.1.1
+   Purpose: return vector where each memeber is a word
+   Input: text file to perform word count on
+   Output: vector with each word separated
+   Note: does not check for delimeters, only spaces.
+		 We can fix that later if we need to
+*/
+	std::vector<std::string> vect;
+	char* word = new char;
+	while (in_file >> word) {
+		vect.push_back(word);
+	}
+	for (int i = 0; i < vect.size(); i++) {
+		cout << vect[i] << endl;
+	}
+	return vect;
+}
+
+std::map<std::string, int> map_func(std::vector<std::string>) {
+
 }
